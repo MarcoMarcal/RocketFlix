44d5a8acd79f32c6a3bc1aa074b884f94a011254
Marco <marquinho09.marcal@gmail.com>
2024-06-01 11:06:56 -0300

    Projeto finalizado

--- template/template/main.js
+++ template/template/main.js
@@ -1,7 +1,7 @@
 const nextMovie = document.getElementById('nextMovie');
 const movies = document.getElementById('movie');
-
 nextMovie.addEventListener('click', findMovie);
+let filmesExibidos = [];
 
 function findMovie() {
   const options = {
@@ -11,21 +11,33 @@ function findMovie() {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODNjMDY4ZGY5NzZlMGE0NjE1YTZiMDcxNjlkNGM4ZSIsInN1YiI6IjY0ZTk3MjI3NTI1OGFlMDBlYWE0NGVmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjMwTv6VnEennnVGTDliCWifVqAq1GlOHPojTr0o5hU'
     }
   };
-
-  fetch('https://api.themoviedb.org/3/movie/popular?language=pt-BR', options)
+  const pmovie = Math.floor(Math.random() * 50)
+  fetch('https://api.themoviedb.org/3/movie/popular?language=pt-BR&page='+ pmovie + '&region=br', options)
     .then(response => response.json())
     .then(data => {
-      movies.innerHTML = ''
-      const randomIndex = Math.floor(Math.random() * data.results.length);
-      const movie = data.results[randomIndex];
+      const filmesDisponiveis = data.results.filter(movie => !filmesExibidos.includes(movie.id));
+
+      if (filmesDisponiveis.length === 0) {
+        filmesExibidos = [];
+      }
+      const randomIndex = Math.floor(Math.random() * filmesDisponiveis.length);
+      const movie = filmesDisponiveis[randomIndex];
+
+      filmesExibidos.push(movie.id);
+
+      movies.innerHTML = '';
+
       const li = document.createElement('li');
       li.innerHTML = `
           <p id="titulo">${movie.title}</p>
+          <div id="divisao">
           <p id="descricao">${movie.overview}</p>
-          <img id="foto" src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></img>
+          <img id="foto" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}"></img>
+          </div>
       `;
       movies.appendChild(li);
       console.log(randomIndex)
     })
     .catch(err => console.error(err));
 }
+
